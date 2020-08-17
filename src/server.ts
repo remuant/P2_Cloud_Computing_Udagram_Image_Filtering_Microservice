a713498646d8c7872fcb47194bf2396265f2b682
Sir Humphrey <62188187+remuant@users.noreply.github.com>
2020-08-17 15:01:22 +0200

    Implemented a restful endpoint

--- src/server.ts
+++ src/server.ts
@@ -9,15 +9,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,15 +27,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get( "/filteredimage", async ( req, res ) => {
+      let { image_url } = req.query;
 
-  //! END @TODO1
-  
+      if (!image_url) {
+        return res.status(422).send({ message: 'Image URL is required!' });
+      }
+
+      let filteredPath = await filterImageFromURL(image_url);
+      res.status(200).sendFile(filteredPath, () => { deleteLocalFiles([filteredPath]); });
+    } );
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
